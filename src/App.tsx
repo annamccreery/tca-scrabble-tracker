3094ac7259002afb5168b07a11781e5078345909
Anna McCreery <amccreery@madisoncollege.edu>
2023-02-24 17:38:20 -0600

    Added dummy components
    added card with bootstrap styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const Home = () => (
+  <>
+    <h1>TCA Scrabble Tracker</h1>
+    <h2>Companion App</h2>
+    <Button variant="outline-primary">Play Scrabble</Button>
+    <Card>
+      <Card.Header>
+        LeaderBoard Stat
+      </Card.Header>
+      <Card.Body>
+        Play a game to see your LeaderBoard
+      </Card.Body>
+    </Card>
+  </>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Home />
     </div>
   );
 }
