064cb93359cdde795e3800aa32e515911f70b962
Anna McCreery <amccreery@madisoncollege.edu>
2023-02-26 13:43:49 -0600

    debuging
    Setup does not appear on github... working on it

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Home } from './Home';
-import { Play} from './Play';
 
+import { Home } from './Home';
+import { Setup } from './Setup';
+import { Play } from './Play';
 
 import {
   HashRouter
-  , Routes
-  , Route,
-  Router
+  , Routes 
+  , Route
 } from 'react-router-dom';
 
 function App() {
@@ -20,6 +20,7 @@ function App() {
       <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/setup" element={<Setup />} />
           <Route path="/play" element={<Play />} />
         </Routes>
       </HashRouter>
