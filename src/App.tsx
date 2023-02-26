155410fa29713af50cf8fb9b70728719f14f33c5
Anna McCreery <amccreery@madisoncollege.edu>
2023-02-26 17:20:39 -0600

    Revert "working out a bug"
    This reverts commit 494b1c861459f9bd281f4cde8780bcc6c85b2202.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Home } from './Home';
 import { Play} from './Play';
-
+import { Setup } from './Setup'
 
 import {
   HashRouter
@@ -20,6 +20,7 @@ function App() {
       <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/setup" element={<Setup />} />
           <Route path="/play" element={<Play />} />
         </Routes>
       </HashRouter>
