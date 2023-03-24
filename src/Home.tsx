8e471a2f3b2e1faa5bbab7a46d269cb37c9e6b84
Anna McCreery <amccreery@madisoncollege.edu>
2023-03-24 13:36:23 -0500

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,14 +3,28 @@ import Card from 'react-bootstrap/Card';
 import { useNavigate } from 'react-router-dom';
 import { LeaderboardPlayer } from './front-end-model';
 import Table from 'react-bootstrap/Table';
+import { durationFormatter } from 'human-readable';
 
 interface HomeProps {
     leaderboardData: LeaderboardPlayer[];
+    shortestGameDuration: number;
+    longestGameDuration: number;
 };
-export const Home: React.FC<HomeProps> = ({leaderboardData}) => {
+export const Home: React.FC<HomeProps> = ({
+    leaderboardData
+    , shortestGameDuration
+    , longestGameDuration
+}) => {
     
-    console.log(leaderboardData);
+    console.log(
+        leaderboardData
+        , shortestGameDuration
+        , longestGameDuration
+        );
+
     const nav = useNavigate();
+
+    const format = durationFormatter();
 
     return (
         <>
@@ -54,6 +68,19 @@ export const Home: React.FC<HomeProps> = ({leaderboardData}) => {
                             </tbody>
                       </Table>
                     }
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Header>
+                    Game Time Fun Facts
+                </Card.Header>
+                <Card.Body>
+                    <p>
+                        {`${format(shortestGameDuration)} shortest game ever`}
+                    </p>
+                    <p>
+                    {`${format(longestGameDuration)} longest game ever`}
+                    </p>
                 </Card.Body>
             </Card>
         </>
