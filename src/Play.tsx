8f8fd6ede47100de80f221b620dda680730ee69c
Anna McCreery <amccreery@madisoncollege.edu>
2023-04-15 15:20:04 -0500

    Formatting buttons on Play page

--- src/Play.tsx
+++ src/Play.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
 import { GameResult, SetupInfo } from './front-end-model';
+import Stack from 'react-bootstrap/Stack';
 
 interface PlayProps {
     addGameResultFunc: (r: GameResult) => void;
@@ -30,18 +31,20 @@ export const Play: React.FC<PlayProps> = ({
     return (
         <div className='bg-light flex-grow-1'>
             <h2 className='mt-2'>Play</h2>
-            <p>Some scoring data here</p>
+            <p className="text-success mt-5">Select the winner!</p>
+            <Stack gap={4} className="col-md-3 mx-auto">
             {
                 setupInfo.chosenPlayers.map(x => (
                     <Button
                         variant="outline-success"
                         onClick={() => endGame(x)}
-                        className="mb-3"
+                        
                     >
                         {x} Won
                     </Button>
                 ))
             }
+            </Stack>
         </div>
     );
 };
